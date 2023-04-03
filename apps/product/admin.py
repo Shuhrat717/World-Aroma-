5fbd2abdcf831bbeb7a6c9d3936f2777cd6af65e
shuhrat <shuhratt000@gmail.com>
2023-04-03 12:42:55 +0500

    product added!

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -4,7 +4,7 @@ from apps.product.models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price',  'created_at')
-    list_display_links = ('id', 'name', 'price',  )
+    list_display_links = ('id', 'name', 'price')
 
 
 admin.site.register(Product, ProductAdmin)
